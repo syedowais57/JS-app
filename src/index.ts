81a454680a7af5682188ada42e1598c39983976f
syedowais57 <owaisquadri8575@gmail.com>
2025-12-04 15:12:50 +0530

    Added Git Ignore

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Owais - Build Success!');
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running at http://localhost:${PORT}`);
+});
