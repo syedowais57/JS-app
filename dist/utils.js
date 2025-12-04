ec4736301a42700ba96db555533e0902319d9399
syedowais57 <owaisquadri8575@gmail.com>
2025-12-04 15:29:32 +0530

    buid check

---
+++ dist/utils.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.greet = greet;
+// Build pass test
+function greet(name) {
+    return `Hello, ${name}! 👋`;
+}
--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Owais - Build Success!');
+  res.sen('Owais - Build Success!');
 });
 
 app.listen(PORT, () => {
