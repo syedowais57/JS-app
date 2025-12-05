ba3b069df317cffbdfcec55aa6389bff9e9ae664
syedowais57 <owaisquadri8575@gmail.com>
2025-12-05 15:33:52 +0530

    modified   index  file v5

--- src/index.ts
+++ src/index.ts
@@ -11,3 +11,15 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log(`🚀 Server running at http://localhost:${PORT}`);
 });
+function greet4(name: string) {
+  return `not, ${name}! 👋`;
+}
+function greet5(name: string) {
+  return `not, ${name}! 👋`;
+}
+function greet6(name: string) {
+  return `not, ${name}! 👋`;
+}
+function greet7(name: string) {
+  return `not, ${name}! 👋`;
+}
